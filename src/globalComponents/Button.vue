5bed79833c7e1b2cf345846e39330f3c71bc8938
fromlovers <danone035@gmail.com>
2020-06-21 03:45:02 +0600

    fix: cleared from bad styles a button

--- src/globalComponents/Button.vue
+++ src/globalComponents/Button.vue
@@ -22,31 +22,16 @@
 </script>
 
 <style lang="scss" scoped>
-    $color-bg: #e0e5ec;
-    $color-shadow: #a3b1c6;
-    $color-white: #ffffff;
-
     .a_button {
-        background-color: $color-bg;
-        text-shadow: 1px 1px 0 $color-white;
         border: 0;
         outline: 0;
-        border-radius: 10px;
+        border-radius: 5px;
         padding: 5px;
         background: #e0e5ec;
-        min-width: 60px;
-        color: #61677c;
+        min-width: 10px;
+        color: #54494b;
         font-weight: bold;
-        box-shadow: -2px -2px 10px $color-white, 2px 2px 10px $color-shadow;
-        transition: all 0.2s ease-in-out;
         cursor: pointer;
-
-        &:hover {
-            box-shadow: -1px -1px 4px $color-white, 1px 1px 4px $color-shadow;
-        }
-
-        &:active {
-            box-shadow: inset 1px 1px 2px $color-shadow, inset -1px -1px 2px $color-white;
-        }
+        color: #ffffff;
     }
 </style>
