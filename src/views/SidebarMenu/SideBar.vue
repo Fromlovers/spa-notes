fc76195063f559fd84264b7f2205eac8ee69de54
fromlovers <danone035@gmail.com>
2020-06-21 03:52:27 +0600

    feat: adds styles and items in component

--- src/views/SidebarMenu/SideBar.vue
+++ src/views/SidebarMenu/SideBar.vue
@@ -5,22 +5,36 @@
         </header>
 
         <div class="sidebar-search">
-            <a-input />
+            <a-input class="sidebar-search-input" placeholder="Search task..." />
         </div>
 
         <div class="sidebar-items">
-            <task />
+            <note :note="note" />
         </div>
-        <!-- <a-button /> -->
+        <div class="sidebar-footer">
+            <a-button label="Add" class="sidebar-footer-button" />
+        </div>
     </div>
 </template>
 
 <script>
     import ToogleButtons from './../Components/ToogleButtons';
-    import Task from '../Components/Task';
+    import Note from '../Components/Note';
+    import { mapActions } from 'vuex';
 
     export default {
-        components: { ToogleButtons, Task },
+        components: { ToogleButtons, Note },
+        methods: {
+            ...mapActions(['getNotes', 'deleteNote']),
+        },
+        computed: {
+            notes() {
+                return this.$store.state.notes;
+            },
+        },
+        created() {
+            this.getNotes();
+        },
     };
 </script>
 
@@ -29,17 +43,37 @@
         display: flex;
         flex-direction: column;
         min-width: 300px;
+        max-width: 350px; //need correct
         margin-right: 20px;
         background: #54494b;
         border: 2px solid rgba(185, 131, 137, 0.6);
         border-radius: 15px;
+        min-height: 550px;
+        cursor: pointer;
 
         &-header {
             display: flex;
-            flex: 2;
+            padding-bottom: 20px;
         }
         &-search {
+            padding-bottom: 14px;
+            &-input {
+                background-color: rgb(103, 93, 91) !important;
+                color: #ffffff !important;
+                height: 2rem !important;
+            }
+        }
+        &-items {
             flex: 1;
+        }
+        &-footer {
+            margin: 10px;
+            &-button {
+                width: 100%;
+                padding: 8px 0 !important;
+                background-color: #519872 !important;
+                border: 0.7px solid rgba(185, 131, 137, 0.6) !important;
+            }
         }
     }
 
