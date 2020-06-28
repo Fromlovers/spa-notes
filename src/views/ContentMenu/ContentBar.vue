bc7b12fa49e7eedddd95aa9ba40cc94a0dd89339
fromlovers <danone035@gmail.com>
2020-06-29 00:19:40 +0600

    feat: welcome window

--- src/views/ContentMenu/ContentBar.vue
+++ src/views/ContentMenu/ContentBar.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="content-container">
-        text example
+        <div class="empty-window">
+            <h2>Hello it's the Notes manager</h2>
+            <h3>You can watch for your time</h3>
+        </div>
     </div>
 </template>
 
@@ -17,10 +20,18 @@
         display: flex;
         flex-direction: column;
         flex: 3;
-        background: #54494b;
         border: 2px solid rgba(185, 131, 137, 0.6);
-        border-radius: 15px;
-        padding: 5px;
+        border-radius: 17px;
+        min-height: 550px;
+        justify-content: center;
+        background: 20% 30% / 100% no-repeat url('../../assets/logo_transparent.png') #54494b;
+        .empty-window {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            color: #ffffff;
+        }
     }
 
     @media (max-width: 799px) {
