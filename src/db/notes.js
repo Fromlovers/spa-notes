2d795e12d6b3d0217f712d3da7639a2bb0d0fe42
fromlovers <danone035@gmail.com>
2020-06-29 00:17:08 +0600

    fix: refactoring vuex and db

--- src/db/notes.js
+++ src/db/notes.js
@@ -1,53 +1,26 @@
-const config = {
-    DB_NAME: 'notes_db',
-    STORAGE_NAME: 'notes',
-    DB_VERSION: 1,
-};
+import getDb from './config/db';
 
-let db;
-
+const storageName = { STORAGE_NAME: 'notes' };
 export default {
-    async getDb() {
-        return new Promise((resolve, reject) => {
-            if (db) {
-                return resolve(db);
-            }
-
-            const request = window.indexedDB.open(config.DB_NAME, config.DB_VERSION);
-            request.onerror = e => {
-                console.error('Error opening db', e);
-                reject('Error');
-            };
-            request.onsuccess = e => {
-                db = e.target.result;
-                console.warn('success');
-                resolve(db);
-            };
-            request.onupgradeneeded = e => {
-                let db = e.target.result;
-                db.createObjectStore(config.STORAGE_NAME, { keyPath: 'id', autoIncrement: true });
-            };
-        });
-    },
     async deleteNote(note) {
-        const db = await this.getDb();
+        const db = await getDb(storageName);
         return new Promise(resolve => {
-            const trans = db.transaction([config.STORAGE_NAME], 'readwrite');
+            const trans = db.transaction([db.config.STORAGE_NAME], 'readwrite');
             trans.oncomplete = () => {
                 resolve();
             };
-            const store = trans.objectStore(config.STORAGE_NAME);
+            const store = trans.objectStore(db.config.STORAGE_NAME);
             store.delete(note.id);
         });
     },
     async getNotes() {
-        let db = await this.getDb();
+        let db = await getDb(storageName);
         return new Promise(resolve => {
-            let trans = db.transaction([config.STORAGE_NAME], 'readonly');
+            let trans = db.transaction([db.config.STORAGE_NAME], 'readonly');
             trans.oncomplete = () => {
                 resolve(notes);
             };
-            const store = trans.objectStore(config.STORAGE_NAME);
+            const store = trans.objectStore(db.config.STORAGE_NAME);
             const notes = [];
             store.openCursor().onsuccess = e => {
                 const cursor = e.target.result;
@@ -60,17 +33,13 @@ export default {
         });
     },
     async saveNote(note) {
-        let db = await this.getDb();
-        console.log(note, 'transaction');
-
+        let db = await getDb(storageName);
         return new Promise(resolve => {
-            let trans = db.transaction([config.STORAGE_NAME], 'readwrite');
+            let trans = db.transaction([db.config.STORAGE_NAME], 'readwrite');
 
-            trans.oncomplete = () => {
-                resolve();
-            };
+            trans.oncomplete = () => resolve();
 
-            let store = trans.objectStore(config.STORAGE_NAME);
+            let store = trans.objectStore(db.config.STORAGE_NAME);
 
             store.put(note);
         });
